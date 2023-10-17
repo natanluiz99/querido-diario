4461b954b3a7f0d52fe3c0ac25d06e1296fce993
NatanLuiz <34273197+natanluiz99@users.noreply.github.com>
2023-10-17 11:12:55 -0300

    Update and rename ba_alcobaca.py to ba_alcobaca_2017.py
    Signed-off-by: NatanLuiz <34273197+natanluiz99@users.noreply.github.com>

--- data_collection/gazette/spiders/ba/ba_alcobaca_2017.py
+++ data_collection/gazette/spiders/ba/ba_alcobaca_2017.py
@@ -1,7 +1,8 @@
 from gazette.spiders.base.doem import DoemGazetteSpider
-
+from datetime import date
 
 class BaAlcobacaSpider(DoemGazetteSpider):
     TERRITORY_ID = "2900801"
     name = "ba_alcobaca"
     state_city_url_part = "ba/alcobaca"
+    start_date = date(2017, 3, 3)
